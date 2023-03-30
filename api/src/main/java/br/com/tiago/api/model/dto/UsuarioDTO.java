e2a9309bcc9af95c07e664d5a99e0ac04d0ebec4
Tiago Silva <tiagobsb84@gmail.com>
2023-03-30 10:06:39 -0300

    Adicionando DTO

---
+++ api/src/main/java/br/com/tiago/api/model/dto/UsuarioDTO.java
@@ -0,0 +1,21 @@
+package br.com.tiago.api.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioDTO {
+
+	private Integer id;
+	private String name;
+	private String email;
+	
+	@JsonIgnore
+	private String password;
+}
--- api/src/main/java/br/com/tiago/api/resource/UserResource.java
+++ api/src/main/java/br/com/tiago/api/resource/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.tiago.api.resource;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.tiago.api.model.Usuario;
+import br.com.tiago.api.model.dto.UsuarioDTO;
 import br.com.tiago.api.service.UsuarioService;
 
 @RestController
@@ -15,10 +16,13 @@ import br.com.tiago.api.service.UsuarioService;
 public class UserResource {
 	
 	@Autowired
+	private ModelMapper mapper;
+	
+	@Autowired
 	private UsuarioService usuarioService;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Usuario> findById(@PathVariable Integer id) {
-		return ResponseEntity.ok().body(usuarioService.findById(id)); 
+	public ResponseEntity<UsuarioDTO> findById(@PathVariable Integer id) {
+		return ResponseEntity.ok().body(mapper.map(usuarioService.findById(id), UsuarioDTO.class)); 
 	}
 }
